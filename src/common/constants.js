b1e1e22f93553c1bc1505004c1cc2b1fc1673617
juhyeon <juhyeon1114@gmail.com>
2023-02-28 12:03:26 +0900

    update : IP filter middleware

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,5 +1,6 @@
 export const PORT = 7000;
 export const ALLOWED_IP = [
     '125.132.211.4', // Laon 회사망
-    '125.132.211.101' // Laon 서버
+    '125.132.211.101', // Laon 서버
+    '118.42.55.126', // 고은 Office
 ];
--- src/middleware/whiteIp.js
+++ src/middleware/whiteIp.js
@@ -1,5 +1,16 @@
+import {ALLOWED_IP} from "../common/constants.js";
+
 const whiteIp = (req, res, next) => {
-    console.log(req.ip);
+    const ip = req.ip;
+    const env = process.env.NODE_ENV;
+    if (env === 'production') {
+        const results = ALLOWED_IP.map(aip => ip.includes(aip));
+        if (results.includes(true)) {
+            next();
+        } else {
+            return res.status(403).json({ error: "허용되지 않은 IP입니다." });
+        }
+    }
     next();
 }
 
