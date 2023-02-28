e71c5706d93307a86245795fb6b7e8a0e94c7d23
juhyeon <juhyeon1114@gmail.com>
2023-02-28 12:06:05 +0900

    update

--- src/middleware/whiteIp.js
+++ src/middleware/whiteIp.js
@@ -2,6 +2,7 @@ import {ALLOWED_IP} from "../common/constants.js";
 
 const whiteIp = (req, res, next) => {
     const ip = req.ip;
+    console.log('IP : ', ip);
     const env = process.env.NODE_ENV;
     if (env === 'production') {
         const results = ALLOWED_IP.map(aip => ip.includes(aip));
